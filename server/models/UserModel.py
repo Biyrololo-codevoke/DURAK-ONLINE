61a167939d8128c0907cc334aa7c446ad5384fc9
codevoke <119005542+codevoke@users.noreply.github.com>
2024-06-16 18:06:04 +0000

    add money field for user

--- server/models/UserModel.py
+++ server/models/UserModel.py
@@ -23,6 +23,7 @@ class UserModel(BaseModel):  # type: ignore
     id = db.Column(db.Integer, primary_key=True)
     _email = db.Column(db.String(120), unique=True)
     username = db.Column(db.String(120), unique=True)
+    money = db.Column(db.Integer, default=100)
     _password = db.Column(db.String(87), nullable=False)
     verified = db.Column(db.Boolean, default=False, nullable=False)
 
@@ -31,6 +32,7 @@ class UserModel(BaseModel):  # type: ignore
             "id": self.id,
             "username": self.username,
             "verified": self.verified,
+            "money": self.money
         }
     
     def __init__(self, email: str, username: str, password: str) -> None:
