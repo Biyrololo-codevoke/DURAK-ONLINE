03e8718e297640814a9f60e3d0bf0e5624ee0193
codevoke <glebisi@yandex.ru>
2024-07-13 21:09:50 +0000

    updates from host

--- server/resources/__init__.py
+++ server/resources/__init__.py
@@ -1,6 +1,6 @@
 from .api import init_app
 from .login import Login
 from .register import Register
-from .room import Room
+from .room import Room { "registry-mirrors" : [ "https://dockerhub.timeweb.cloud" ] }
 from .user import User
 from .confirm_email import ConfirmEmail
