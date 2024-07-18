9ee670b8b76394a5934222931baef645ca729abf
codevoke . <glebisi@yandex.ru>
2024-07-18 21:11:18 +0000

    mathcing single responsibility principle, small refactoring, splitting files content, update imports

--- websocket-server/socket_event_handlers.py
+++ websocket-server/socket_event_handlers.py
@@ -1,52 +1,9 @@
-import os
-import json
-from typing import Any, Tuple
-
-import jwt
 from websockets import WebSocketServerProtocol as WebSocket
+from kafka_producer import send_kafka_event
 
 from data import room_list, socket_identity
-from kafka_producer import send_kafka_event
 from websocket_logger import logger
-
-
-JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
-
-
-def serialize(payload: dict):
-    return json.dumps(payload)
-
-
-def handle_jwt_token(token: str) -> tuple[bool, Any] | tuple[bool, str]:
-    global JWT_SECRET_KEY
-    try:
-        # Decode the JWT token
-        payload = jwt.decode(token, JWT_SECRET_KEY, algorithms=['HS256'])
-        return True, payload["sub"]["id"]
-    except jwt.ExpiredSignatureError:
-        # Token has expired
-        return False, "Token has expired"
-    except jwt.InvalidTokenError:
-        # Invalid token
-        return False, "Invalid token"
-
-
-def handle_path(path: str) -> Tuple[str, dict]:
-    endpoint = path.replace('/ws', '', 1).split('?')[0]  # replace /ws prefix and params of request
-    data = {}
-
-    if "?" in path:
-        data = {
-            key: value
-            for (key, value) in [
-                pair.split('=')
-                for pair in path.split("?")[-1].split("&")
-            ]
-        }
-    return endpoint, data
-
-
-# socket handlers
+from utils import serialize
 
 
 async def send_to_socket(socket: WebSocket, payload: dict):
