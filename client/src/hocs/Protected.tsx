e4b189c3fc9e530bead190b7c42a0ac2656483a1
biyrololo <92595932+biyrololo@users.noreply.github.com>
2024-07-21 15:38:18 +0000

    add protected pages again

--- client/src/hocs/Protected.tsx
+++ client/src/hocs/Protected.tsx
@@ -11,12 +11,12 @@ export default function Protected({ children }: Props) {
     const access_token = Cookies.get('access_token');
     
     useEffect(() => {
-        if (!access_token && 0) {
+        if (!access_token) {
             navigate('/login')
         }
     }, [access_token, navigate])
 
-    if (!access_token && 0) {
+    if (!access_token) {
         return null
     }
 
