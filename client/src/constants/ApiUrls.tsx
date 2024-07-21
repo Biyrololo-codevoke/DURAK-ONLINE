72578294977e29129fd8c6f77b57b79e378b4cbf
biyrololo <92595932+biyrololo@users.noreply.github.com>
2024-07-21 14:27:37 +0000

    room/list -> room-list

--- client/src/constants/ApiUrls.tsx
+++ client/src/constants/ApiUrls.tsx
@@ -23,7 +23,7 @@ function uploadPhoto(){
 }
 
 function roomListWS(){
-    return `/ws/room/list` // wss://codevoke.ru/ws/room/list
+    return `/ws/room-list` // wss://codevoke.ru/ws/room/list
 }
 
 export {roomListWS}
