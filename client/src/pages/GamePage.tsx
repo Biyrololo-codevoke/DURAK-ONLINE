889d58cc5f90d94f7d9d108122b0d966f78ed2b2
biyrololo <92595932+biyrololo@users.noreply.github.com>
2024-08-04 21:02:19 +0400

    updates

--- client/src/pages/GamePage.tsx
+++ client/src/pages/GamePage.tsx
@@ -811,6 +811,42 @@ export default function GamePage(){
         socket_ref.current.send(
             JSON.stringify(data)
         )
+
+        const _game_board : GameBoardCard[] = JSON.parse(localStorage.getItem('_game_board') || '[]');
+
+        const _users_ids : UserIdType[] = JSON.parse(localStorage.getItem('_users_ids') || '[]');
+    
+        const _game_players : GamePlayers = JSON.parse(localStorage.getItem('game_players') || '{}');
+
+        const _user_id = parseInt(localStorage.getItem('user_id') || '-1');
+
+        setTimers(prev => {
+            const new_timers : Timer[] = [];
+
+            for(let id of _users_ids){
+                let _id = id;
+                if(_id === 'me') _id = _user_id;
+
+                if(_id === _game_players.victim){
+                    new_timers.push({
+                        id: _id,
+                        color: 'green',
+                        from_start: true,
+                        is_active: true
+                    })
+                }
+                else{
+                    new_timers.push({
+                        id: _id,
+                        color: 'red',
+                        from_start: true,
+                        is_active: false
+                    })
+                }
+            }
+
+            return new_timers;
+        })
     }
 
     // transfer cards
