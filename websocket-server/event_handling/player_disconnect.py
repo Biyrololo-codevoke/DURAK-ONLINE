ab4adba2a3a4cc7ee93a6468545c38f8cea3b0b3
root <root@qgzjvotijo.local>
2024-12-19 10:00:24 +0000

    update disconnect feature

--- websocket-server/event_handling/player_disconnect.py
+++ websocket-server/event_handling/player_disconnect.py
@@ -1,6 +1,8 @@
-from .data import Player
+from .data import Player, room_list
 
 
 def handle_disconnect(player: Player):
     if player.type == "ROOM":
         player.broadcast_room("player_disconnect", {"id": player.id})
+    else:
+        room_list.unsubscribe(player)
